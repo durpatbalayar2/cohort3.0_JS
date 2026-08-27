6975cf977ec4e932d34475132b46cd75fff7af9b
Durpat <durpatbalayar2@gmail.com>
2026-08-27 22:31:43 +0530

    JS phase 1

---
+++ JS_phase_1/app.js
@@ -0,0 +1,80 @@
+// Console & Basic
+
+//solution:
+//1
+// console.log("Hello world");
+
+//2
+
+// let nm = "Durpat";
+// let age = 25;
+// let city = "benguluru";
+
+// console.log("Name:", nm, "age:", age, "city:", city);
+
+//3.
+// console.warn("please donot click on it!");
+
+//4.
+// console.error("This page is not exist");
+
+//5.
+// let num = [2, 4, 6, 8, 10];
+// console.table(num);
+
+//## Variables
+
+// 1. Create a variable called `studentName` and store your name in it.
+// 2. Create a variable `age` and print it.
+// 3. Create two variables and swap their values.
+// 4. Create a constant variable for `PI` and print it.
+// 5. Declare a variable without assigning a value and print it.
+// 6. Create a variable `score` and increase it by 10.
+// 7. Create three variables for first name, last name, and full name.
+
+//1.
+
+// let studentNAme;
+// studentNAme = "Durpat";
+// console.log(studentNAme);
+
+//2.
+
+// let age = 20;
+// console.log("your age is ;", age);
+
+//3.
+
+// let a = 20;
+// let b = 10;
+
+// a = a + b; //30 -> a
+// b= a - b; //20 -> b
+// a = a - b;//10
+// console.log(a);
+// console.log(b);
+
+//4.
+
+// const PI = 3.14
+
+// console.log(PI)
+
+//5.
+
+// let age;
+// console.log(age); // undefined
+
+//6.
+
+// let score = 0;
+
+// score += 10;
+// console.log(score);
+
+//7.
+
+// let firstname = "Ram";
+// let lastname = "Sita";
+// let fullname = firstname +" "+ lastname;
+// console.log(fullname)
