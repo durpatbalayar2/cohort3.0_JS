d50017f51e7020c340d5d5fcca5b0ebfeab9ccab
Durpat <durpatbalayar2@gmail.com>
2026-08-29 23:46:02 +0530

    lvl1 done

--- JS_phase_1/app.js
+++ JS_phase_1/app.js
@@ -1,26 +1,62 @@
-// 7. Ask user’s age and check if eligible to vote
-// If age >= 18 → “Eligible”, else → “Not eligible”
+// 8. Print multiplication table of 5
+// Use loop to print 5 × 1 to 5 × 10.
 
-let age = prompt("Enter your age:");
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`5 X ${i} = ${5 * i}`);
+// }
 
-if (age === null) {
-  console.warn("You cancelled");
-} else if (age.trim() === "") {
-  console.error("You have not entered the number");
+// 9. Count how many numbers between 1 and 15 are greater than 8
+// Loop and count conditionally.
+
+// let count = 0;
+// for (let i = 1; i <= 15; i++) {
+//   if (i > 8) {
+//     count++;
+
+//     console.log(i);
+//   }
+// }
+// console.log(`Total count are ${count}`);
+
+// 10. Ask user for password and print access status
+// Hardcoded correct password. Compare with user input.
+
+let password = "heyhey";
+
+let pass = prompt("Enter your password");
+
+if (password === pass) {
+  console.log("Account opened");
 } else {
-  age = age.trim();
-
-  if (isNaN(age)) {
-    console.error("Please enter the age number");
-  } else {
-    age = Number(age);
-
-    if (age < 0 || age > 100) {
-      console.log("This is not valid age");
-    } else if (age >= 18) {
-      console.log(`Your age is ${age} and eligible for vote`);
-    } else {
-      console.log(` Age is ${age} and not eligible for vote`);
-    }
-  }
+  console.log("Account locked");
 }
+
+// Level 2 – Slightly Tougher but Logical
+// 11. Allow only 3 attempts to enter correct password
+// If user gets it right early, stop. If not → “Account locked”
+
+// 12. Ask user for words until they type “stop”. Count how many times they typed “yes”
+// Loop until "stop" is typed. Count "yes".
+
+// 13. Print numbers divisible by 7 from 1 to 50
+// Use modulo % and loop.
+
+// 14. Sum of all odd numbers from 1 to 30
+// Add only odd numbers. Print final sum.
+
+// 15. Keep asking number until user enters an even number
+// Use while loop. Stop only if input is even.
+
+// 16. Print numbers between two user inputs
+// Input start and end using prompt() → print all between.
+
+// 17. Print only first 3 odd numbers from 1 to 20
+// Use loop. Stop with break after 3 odd prints.
+
+// 18. Ask user 5 numbers. Count how many are positive
+// Use loop + condition + counter.
+
+// 19. ATM Simulator – Allow 3 withdrawals
+// Start with ₹1000 balance. Ask withdrawal amount 3 times.
+// If enough balance → deduct
+// Else → print “Insufficient balance”
